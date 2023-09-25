6ad4fde2d72ed6a4089bbf3c2207b8b8e749acdb
Udit <royalboy0012@gmail.com>
2023-09-25 22:31:57 +0530

    env variable fixed

--- backend/models/db.js
+++ backend/models/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose"
+import dotenv from "dotenv"
+dotenv.config()
 
 const mongoConnect = ()=>{
-    mongoose.connect('mongodb+srv://UditGoyal0012:Royalboy0012@cluster0.seioabj.mongodb.net/?retryWrites=true&w=majority').then(()=>{console.log("Connected to database successfully")})
+    mongoose.connect(process.env.MONGO_URI).then(()=>{console.log("Connected to database successfully")})
 }
 
 export default mongoConnect
